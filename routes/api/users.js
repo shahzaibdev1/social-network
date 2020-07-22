c626892b6d60330aa5737050939884b5d157dc25
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-07-22 10:27:21 +0500

    added register route in users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const User = require("../../config/models/User");
 const router = express.Router();
 
 // @route   Get api/users/test
@@ -8,4 +9,14 @@ router.get("/test", (req, res) =>
   res.json({ msg: "Users router is working..." })
 );
 
+// @route Get /api/users/register
+// @desc  Registers User
+// access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      return res.status(400);
+    }
+  });
+});
 module.exports = router;
