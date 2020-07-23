dfae21607ce5c0cb80088cb48e5b73eb3987a442
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-07-23 14:52:54 +0500

    Updated passport.js added real functionality

--- config/passport.js
+++ config/passport.js
@@ -12,7 +12,15 @@ opts.secretOrKey = keys.secretOrKey;
 module.exports = (passport) => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
-      console.log(jwt_payload);
+      User.findById(jwt_payload.id)
+        .then((user) => {
+          if (user) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        })
+        .catch((err) => console.log(err));
     })
   );
 };
