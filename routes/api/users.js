8b5b9cef400d5a31948519cf82bd37dd848007a8
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-07-23 14:41:09 +0500

    Added /current route to users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,7 +5,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-const { secret } = require("../../config/keys");
+const passport = require("passport");
 
 // @route   Get api/users/test
 // @desc    Tests users Route
@@ -73,17 +73,33 @@ router.post("/login", (req, res) => {
         const payload = { id: user.id, name: user.name, avatar: user.avatar }; // JWT payload
 
         // Sign Token
-        jwt.sign(payload, keys.secret, { expiresIn: 3600 }, (err, token) => {
-          res.json({
-            success: true,
-            token: "Bearer " + token,
-          });
-        });
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token,
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Incorrect Password" });
       }
     });
   });
 });
+
+// @route Get /api/users/current
+// @desc  Passport-JWT | session functionality
+// access Public
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({ msg: "Success" });
+  }
+);
 
 module.exports = router;
