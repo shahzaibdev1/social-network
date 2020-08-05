9da39ff37e617dfbca24854d44688ff716c4fc4e
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-05 12:53:44 +0500

    Updated readablity and indentation for Post model

--- models/Post.js
+++ models/Post.js
@@ -7,16 +7,20 @@ const PostSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "users",
   },
+
   text: {
     type: String,
     required: true,
   },
+
   name: {
     type: String,
   },
+
   avatar: {
     type: String,
   },
+
   likes: [
     {
       user: {
@@ -25,28 +29,34 @@ const PostSchema = new Schema({
       },
     },
   ],
+
   comments: [
     {
       user: {
         type: Schema.Types.ObjectId,
         ref: "users",
       },
+
       text: {
         type: String,
         required: true,
       },
+
       name: {
         type: String,
       },
+
       avatar: {
         type: String,
       },
+
       date: {
         type: Date,
         default: Date.now,
       },
     },
   ],
+
   date: {
     type: Date,
     default: Date.now,
