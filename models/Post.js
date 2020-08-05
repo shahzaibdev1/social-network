c7f2bde4052a92bbb857f43f8dbc55ab4b7f8873
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-05 12:49:19 +0500

    update Post model a bit

--- models/Post.js
+++ models/Post.js
@@ -1,31 +1,22 @@
 const mongoose = require("mongoose");
-const { post } = require("../routes/api/profile");
 const Schema = mongoose.Schema;
 
+// Create Schema
 const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "users",
   },
-
   text: {
     type: String,
     required: true,
   },
-
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-
   name: {
     type: String,
   },
-
   avatar: {
     type: String,
   },
-
   likes: [
     {
       user: {
@@ -47,10 +38,19 @@ const PostSchema = new Schema({
       name: {
         type: String,
       },
-      avatar: { type: String },
-      date: { type: Date, default: Date.now },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
     },
   ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = Post = mongoose.model("post", PostSchema);
