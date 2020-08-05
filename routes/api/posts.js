e0efbc6bea9f9861377b4272074cdb63822e523e
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-05 14:02:42 +0500

    added like and unlike functionality to posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -83,7 +83,7 @@ router.delete(
 // @desc    Like the post
 // @access  Private
 router.post(
-  "like/:id",
+  "/like/:id",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     Profile.findOne({ user: req.user.id }).then((profile) => {
@@ -99,6 +99,40 @@ router.post(
           }
 
           post.likes.unshift({ user: req.user.id });
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postNotFound: "Post not found." })
+        );
+    });
+  }
+);
+
+// @route   POST api/posts/like/:id
+// @desc    Like the post
+// @access  Private
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: "You have not liked the post yet" });
+          }
+
+          let removeIndex = post.likes
+            .map((item) => item.user.toString())
+            .indexOf(req.user.id);
+
+          post.likes.splice(removeIndex, 1);
+
           post.save().then((post) => res.json(post));
         })
         .catch((err) =>
