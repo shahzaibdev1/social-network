5e87dfeeb0cff92fd5ad0b1431797204252d04f3
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-05 13:29:25 +0500

    Added like post functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -65,41 +65,47 @@ router.delete(
     Profile.findOne({ user: req.user.id }).then((profile) => {
       Post.findById(req.params.id)
         .then((post) => {
-          // Check for post owner
           if (post.user.toString() !== req.user.id) {
             return res
               .status(401)
-              .json({ notauthorized: "User not authorized" });
+              .json({ notAuthorized: "User not authorized" });
           }
-
-          // Delete
           post.remove().then(() => res.json({ success: true }));
         })
         .catch((err) =>
-          res.status(404).json({ postnotfound: "No post found" })
+          res.status(404).json({ postNotFound: "Post not found." })
         );
     });
   }
 );
-// router.delete(
-//   "/:id",
-//   passport.authenticate("jwt", { session: false }),
-//   (req, res) => {
-//     Profile.findOne({ user: req.user.id }).then((profile) => {
-//       Post.findById(req.params.id)
-//         .then((post) => {
-//           if (post.user.toString() !== req.user.id) {
-//             return res
-//               .status(401)
-//               .json({ notAuthorized: "User not authorized" });
-//           }
-//           post.remove().then(() => res.json({ success: true }));
-//         })
-//         .catch((err) =>
-//           res.status(404).json({ postNotFound: "Post not found." })
-//         );
-//     });
-//   }
-// );
+
+// @route   POST api/posts/like/:id
+// @desc    Like the post
+// @access  Private
+router.post(
+  "like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then((profile) => {
+      Post.findById(req.params.id)
+        .then((post) => {
+          if (
+            post.likes.filter((like) => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: "User liked the post already" });
+          }
+
+          post.likes.unshift({ user: req.user.id });
+          post.save().then((post) => res.json(post));
+        })
+        .catch((err) =>
+          res.status(404).json({ postNotFound: "Post not found." })
+        );
+    });
+  }
+);
 
 module.exports = router;
