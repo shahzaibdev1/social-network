fcad6a58c87b1235f29aa0995156f4cf837b3835
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-11 19:09:25 +0500

    Added font-awesome

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
