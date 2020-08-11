a0bf7fc728bb2ef5ac1bb56689346a56a06a1c1e
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-11 19:44:43 +0500

    Initial simple routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
       <div className="App">
         <Navbar />
         <Route path="/" exact component={Landing} />
+        <div className="container">
+          <Route path="/login" exact component={Login} />
+          <Route path="/register" exact component={Register} />
+        </div>
         <Footer />
       </div>
     </Router>
