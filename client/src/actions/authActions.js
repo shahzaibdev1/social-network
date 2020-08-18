75da6b56d2f600beca016b3faae1cd8a4beb2b41
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-18 13:50:47 +0500

    Added loginUser Action for logIn in redux

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import setAuthToken from "../utils.js/setAuthToken";
 import { GET_ERRORS } from "./types";
+import { SET_CURRENT_USER } from "./types";
+import jwt_decode from "jwt-decode";
 
 //Register the user
 export const registerUser = (userData, history) => (dispatch) => {
   axios
-    .post(`http://127.0.0.1:5000/api/users/register`, userData)
+    .post(`/api/users/register`, userData)
     .then((res) => history.push("/login"))
     .catch((err) =>
       dispatch({
@@ -12,4 +15,35 @@ export const registerUser = (userData, history) => (dispatch) => {
         payload: err.response.data,
       })
     );
+};
+
+// Login the user - Get the the JWT token
+export const loginUser = (userData) => (dispatch) => {
+  axios
+    .post("/api/users/login", userData)
+    .then((res) => {
+      // get token from res object
+      const { token } = res.data;
+      // save token to localStorage
+      localStorage.setItem("jwtToken", token);
+      // set token to auth header
+      setAuthToken(token);
+      // Decode the user token
+      const decoded = jwt_decode(token);
+      dispatch(setCurrentUser(decoded));
+    })
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// set login user
+export const setCurrentUser = (decoded) => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded,
+  };
 };
