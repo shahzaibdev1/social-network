ef87b5feb4159939c0ac0a02c36e3c2ed7e2f113
Shahzaib Umaar <shahzaibumaar953@gmail.com>
2020-08-21 21:57:27 +0500

    Added dispatch to logout

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import store from "./store";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils.js/setAuthToken";
-import { setCurrentUser } from "./actions/authActions";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.css";
@@ -17,9 +17,23 @@ import Register from "./components/auth/Register";
 
 // Check for token
 if (localStorage.jwtToken) {
+  // Set auth token header auth
   setAuthToken(localStorage.jwtToken);
+  // Decode token and get user info and exp
   const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // TODO: Clear current Profile
+
+    // Redirect to login
+    window.location.href = "/login";
+  }
 }
 
 class App extends Component {
