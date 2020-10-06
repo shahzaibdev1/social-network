e529d8ebf9074af4232ff02c3ff844124efaf56a
Shahzaib <shahzaibumaar953@gmail.com>
2020-10-06 18:04:28 +0500

    Little changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -81,7 +81,7 @@ class Navbar extends Component {
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item">
                   <Link className="nav-link" to="/profiles">
-                    Developers
+                    Profiles
                   </Link>
                 </li>
               </ul>
