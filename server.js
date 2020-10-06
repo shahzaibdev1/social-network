8a959f2dcff505c42a3dac4c219fefbe31448e4b
Shahzaib <shahzaibumaar953@gmail.com>
2020-10-06 13:18:22 +0500

    Prepared for deployment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 const passport = require("passport");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -39,6 +40,15 @@ require("./config/passport")(passport);
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port: ${port}`));
