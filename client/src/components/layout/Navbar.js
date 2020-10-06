45e61dae05aed552a77a4e7b0862f94a9620dcf4
Shahzaib <shahzaibumaar953@gmail.com>
2020-10-06 18:41:12 +0500

    Little Changes

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -16,13 +16,13 @@ class Navbar extends Component {
 
     const authLinks = (
       <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
+        <li className="nav-item my-auto">
           <Link className="nav-link" to="/dashboard">
             Dashboard
           </Link>
         </li>
 
-        <li className="nav-item">
+        <li className="nav-item my-auto">
           <Link className="nav-link" to="/feed">
             Post Feed
           </Link>
@@ -48,12 +48,12 @@ class Navbar extends Component {
 
     const guestLinks = (
       <ul className="navbar-nav ml-auto">
-        <li className="nav-item">
+        <li className="nav-item my-auto">
           <Link className="nav-link" to="/register">
             Sign Up
           </Link>
         </li>
-        <li className="nav-item">
+        <li className="nav-item my-auto">
           <Link className="nav-link" to="/login">
             Login
           </Link>
@@ -78,13 +78,15 @@ class Navbar extends Component {
             </button>
 
             <div className="collapse navbar-collapse" id="mobile-nav">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/profiles">
-                    Profiles
-                  </Link>
-                </li>
-              </ul>
+              <div>
+                <div className="navbar-nav mr-auto">
+                  <p className="nav-item my-auto">
+                    <Link className="nav-link" to="/profiles">
+                      Profiles
+                    </Link>
+                  </p>
+                </div>
+              </div>
               {isAuthenticated ? authLinks : guestLinks}
             </div>
           </div>
